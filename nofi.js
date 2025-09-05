77c3228b90d7813be7ea8638b7eee479867946df
Abhishek Upadhyay <abhishek.20scse1010430@galgotiasuniversity.edu.in>
2025-09-05 11:09:45 +0530

    Add files via upload

---
+++ nofi.js
@@ -0,0 +1,12 @@
+const title =  "Da6:sample Code";
+console.log(title);
+title="Day7: sample Code Updated";
+console.log(title);
+
+
+
+let num1 = 25;
+
+if(num > 0)  {
+    console.log("number is positive");
+}
